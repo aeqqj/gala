63a2235f490a0c7b987dee4ef6fc7314774dc3c9
aeqqj <48713075+aeqqj@users.noreply.github.com>
2026-06-17 14:01:40 +0800

    chore: frontend setup finished

---
+++ src/app/app.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <section className="h-screen w-screen flex justify-center items-center">
+        HALLOO
+      </section>
+    </>
+  )
+}
+
+export default App;
